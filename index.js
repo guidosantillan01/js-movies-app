f8cbd003d940956d914b34464abf65b5b7539e05
Guido Santillan <guidosantillan01@gmail.com>
2019-12-31 14:34:55 +0100

    Handling empty responses

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async e => {
   const movies = await fetchData(e.target.value);
 
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
+
   resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
 
